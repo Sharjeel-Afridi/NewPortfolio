5435c312a9fb8ed4303df165ed4a4be23562b7aa
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2023-10-12 00:55:45 +0530

    added a return button in 404

---
+++ 404script.js
@@ -0,0 +1,6 @@
+const returnBtn =  document.getElementById('returnBtn');
+
+returnBtn.addEventListener('click', function (){
+    const url = "https://sharjeelafridi.netlify.app";
+    window.location.href = url;
+});
